d71f34128e7cc3d7f74b17a24c734586583d7bd0
tanxunrong <tanxr@tanxunrongtekiiMac.local>
2013-09-28 13:55:15 +0800

    ghashtable

--- glibtest/main.c
+++ glibtest/main.c
@@ -10,41 +10,30 @@
 #include <glib.h>
 #define TXR_MALLOC(type) ((type *)malloc(sizeof(type)))
 
-
 typedef struct txr_conn{
     int sockfd;
+    char key[20];
     char buf[MAXLINE];
 }txr_conn_t;
 typedef txr_conn_t * txr_conn_p;
-void seq_del(gpointer *data)
-{
-    printf("seq element del\n");
-}
-void seq_foreach(gpointer data)
-{
-    txr_conn_p conn = (txr_conn_p)data;
-    printf("%d\n",conn->sockfd);
-}
-static inline gint seq_comp(gconstpointer data1,gconstpointer data2,gpointer userdata)
-{
-    return ((txr_conn_p)data1)->sockfd -
-        ((txr_conn_p)data2)->sockfd;
-}
 int main(int argc,char* argv[])
 {
-    GSequence *connlist=g_sequence_new((GDestroyNotify)seq_del);
+    GHashTable *connht=g_hash_table_new_full(g_str_hash, g_str_equal,NULL,free);
 #define LEN_OF_CONNLIST 10
     txr_conn_p conn[LEN_OF_CONNLIST];
     for (int i=0; i<LEN_OF_CONNLIST; i++) {
         conn[i]=(txr_conn_p)malloc(sizeof(txr_conn_t));
         memset(conn[i], 0, sizeof(txr_conn_t));
         conn[i]->sockfd=rand();
-        g_sequence_append(connlist, conn[i]);
+        sprintf(conn[i]->key, "conn list %d",i);
+        printf("key %d : %s\n",i,conn[i]->key);
+        g_hash_table_insert(connht, conn[i]->key, conn[i]);
     }
-    printf("length %d\n",g_sequence_get_length(connlist));
-    g_sequence_foreach(connlist, (GFunc)seq_foreach, NULL);
-    g_sequence_sort(connlist,seq_comp,NULL);
-    g_sequence_foreach(connlist, (GFunc)seq_foreach, NULL);
+    assert(10 == g_hash_table_size(connht));
+    txr_conn_p connfind = g_hash_table_lookup(connht, "conn list 8");
+    assert(connfind != NULL);
+    assert(TRUE == g_hash_table_contains(connht, "conn list 7"));
+    g_hash_table_destroy(connht);
     return 0;
 }
 
