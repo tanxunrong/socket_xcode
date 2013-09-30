5971c42d7c83c190d989ccdbf6dc9500f798a745
tanxunrong <tanxr@tanxunrongtekiiMac.local>
2013-10-01 00:41:13 +0800

    thread pool test

--- glibtest/main.c
+++ glibtest/main.c
@@ -8,6 +8,7 @@
 
 #include "../include/common.h"
 #include <glib.h>
+
 #define TXR_MALLOC(type) ((type *)malloc(sizeof(type)))
 
 typedef struct txr_conn{
@@ -16,28 +17,34 @@ typedef struct txr_conn{
     char buf[MAXLINE];
 }txr_conn_t;
 typedef txr_conn_t * txr_conn_p;
-gint conncmp(gconstpointer data1,gconstpointer data2)
+static int sum = 0;
+void sumtohuge(gpointer data,gpointer userdata)
 {
-    return ((txr_conn_p)data1)->sockfd - ((txr_conn_p)data2)->sockfd;
+    int upnum = *(int *)data;
+    printf("%d add\n",upnum);
+    sum += upnum;
+    int sltime = rand() % 10;
+    sleep(sltime);
 }
 int main(int argc,char* argv[])
 {
-#define LEN_OF_CONNLIST 10
-    GArray *connarr=g_array_sized_new(FALSE, TRUE, sizeof(txr_conn_t),LEN_OF_CONNLIST);
-    txr_conn_p conn = NULL;
-    for (int i=0; i<LEN_OF_CONNLIST; i++) {
-        conn = &g_array_index(connarr, txr_conn_t, i);
-        conn->sockfd=rand() % 100;
+    GError *err;
+    GThreadPool *sumpool = g_thread_pool_new(sumtohuge,NULL,10,TRUE,&err);
+    assert(err == NULL);
+//    for (int i=0; i<100; i++) {
+//        int upnum = rand() % 100;
+//        printf("upnum %d\t",upnum);
+//        g_thread_pool_push(sumpool, &upnum, &err);
+//        assert(err == NULL);
+//    }
+    for (int i=0; i<100; i++) {
+        int upnum = rand() % 100;
+        g_thread_pool_push(sumpool, &upnum, &err);
+        assert(err == NULL);
     }
-    for (int i=0; i<LEN_OF_CONNLIST; i++) {
-        conn = &g_array_index(connarr, txr_conn_t, i);
-        printf("%d\n",conn->sockfd);
-    }
-    g_array_sort(connarr, (GCompareFunc)conncmp);
-    for (int i=0; i<LEN_OF_CONNLIST; i++) {
-        conn = &g_array_index(connarr, txr_conn_t, i);
-        printf("%d\n",conn->sockfd);
-    }
+    printf("%d unprocessed\n",g_thread_pool_unprocessed(sumpool));
+    g_thread_pool_free(sumpool, FALSE, TRUE);
+    printf("sum %d\n",sum);
     return 0;
 }
 
